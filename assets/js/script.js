d5cc62d44684a795ea3af634a1a0cac97e20431c
fortu038 <fortu038@umn.edu>
2022-09-22 18:38:36 -0500

    updated event listener and question building function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-var start_button = document.querySelector("#start");
+var high_scores_button = document.querySelector("#high_scores_button");
+
+var timer = document.querySelector("#timer");
+
+var start = document.querySelector("#start");
+
+var question_and_answers = document.querySelector("#question_and_answers");
+var question = document.querySelector("#question");
+var answers = document.querySelector("#answers");
+
+var high_scores_list = document.querySelector("#high_scores_list");
+
 
 var current_score = 0;
-var seconds_remaining = 120;
-var high_scores = [];
+var seconds_remaining = 60;
+
+var high_scores = []; // <- Have this be an array of arrays that stores nicknames and scores as pairs
+var top_score = 0;
+var low_score = 0;
+
 
 function init() {
     var scores = JSON.parse(localStorage.getItem("high_scores"));
@@ -18,15 +33,47 @@ function end_game() {
 
 }
 
-function show_question() {
-    // Figure which item to get from the array whenever this function is called (use a global?)
-    var curr_question_obj = questions[i];
-    var section = document.createElement("section");
-    // create an h2 tag, give it the text of the question
-    // create a ul/ol tag
-    // for each answers, create an li tag
+// function show_question() {
+//     // Figure which item to get from the array whenever this function is called (use a global?)
+//     var curr_question_obj = questions[i];
+//     var section = document.createElement("section");
+//     // create an h2 tag, give it the text of the question
+//     // create a ul/ol tag
+//     // for each answers, create an li tag
 
-    // add all this stuff to the DOM
+//     // add all this stuff to the DOM
+// }
+
+function build_question(q_num) {
+    var curr_question_obj = questions_array[q_num];
+    question.innerHTML = curr_question_obj.q;
+    var len_as = curr_question_obj.as.length;
+    for(var i = 0; i < len_as; i++) {
+        var li_tag = document.createElement("li");
+        li_tag.setAttribute("id", curr_question_obj.as[i][1]);
+        
+        li_tag.appendChild(document.createTextNode(curr_question_obj.as[i][0]));
+ 
+        answers.appendChild(li_tag);
+    }
+
+    var answers_array = document.querySelectorAll("#answers");
+
+    console.log(answers);
+    console.log(answers_array);
+
+    var ans_buttons = document.querySelectorAll("#answers")
+
+    for(var i = 0; i < ans_buttons.length; i++) {
+        ans_buttons[i].addEventListener("click", function(event) {
+            var isCorrect = event.target.getAttribute("id");
+            if(isCorrect == "true") {
+                alert("Correct!");
+            } else {
+                alert("Wrong");
+            }
+        });
+    }
 }
 
 // When the user clicks start, the follow needs to happen
@@ -52,21 +99,21 @@ for(var i = 0; i < buttons.length; i++) {
 
 var questions_array = [
     {
-        question: "How many fingers do humans have?",
-        answers: [
-            [1,false],
-            [2,false],
-            [3,false],
-            [10,true]
+        q: "How many fingers do humans have?",
+        as: [
+            [1, false],
+            [10, true],
+            [2, false],
+            [3, false]
         ],
     },
 
     {
-        question: "What is the northern-most state in the US?",
-        answers: [
+        q: "What is the northern-most state in the US?",
+        as: [
             ["Hawaii",false],
-            ["Nevada",false],
-            ["Maryland",false],
+            ["Minnesota",false],
+            ["Guam",false],
             ["Alaska",true]
         ],
     }
@@ -83,8 +130,12 @@ for(var i = 0; i < questions_array.length; i++) {
 }
 
 // Event listener for clicking the start button
-start_button.addEventListener("click", function() {
-    console.log("clicked start");
+start.addEventListener("click", function() {
+    // console.log("clicked start");
+    start.style.display = "none";
+    question_and_answers.style.display = "flex";
+    // console.log(questions_array[0].q);
+    build_question(0);
 });
 
 // Event listener for choosing an answer
